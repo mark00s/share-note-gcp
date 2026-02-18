d4b60ed0debd7d1980fae256e18cb04a731c78fd
mark00s <marek.jakobiak@gmail.com>
2026-02-18 21:02:35 +0100

    chore: Dumped improvements into TODOs for later.

--- 02_backend/envs.py
+++ 02_backend/envs.py
@@ -1,6 +1,7 @@
 import os
 
 # Defaults are for dev development
+# TODO: https://docs.pydantic.dev/latest/concepts/pydantic_settings/#usage
 APP_API_KEY = os.getenv("APP_API_KEY", "D3VM0DE")
 FRONTEND_ADDRESS = os.getenv("FRONTEND_ADDRESS", "http://localhost:4200")
 FIRESTORE_DB_COLLECTION = os.getenv("FIRESTORE_DB_COLLECTION", "dev_notes")
--- 02_backend/models.py
+++ 02_backend/models.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from constants import NOTE_TTL
 
 
+# TODO: https://docs.pydantic.dev/2.0/usage/types/secrets/
 class CreateNote(BaseModel):
     content: str
     password: str = ""  # No password by default
