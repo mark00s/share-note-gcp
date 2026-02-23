01e4db351240723affe8ff59eb1047ee49e2820e
mark00s <marek.jakobiak@gmail.com>
2026-02-23 10:39:11 +0100

    feat: Added environment files

---
+++ 01_frontend/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: true,
+	firebase: {
+		// TODO: Firebase config
+	},
+	backendUrl: "", // Empty = use Cloud Run URL from Firebase config
+	useEmulators: false,
+};
---
+++ 01_frontend/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+	production: false,
+	firebase: {
+		// TODO: Your Firebase config
+	},
+	backendUrl: "http://localhost:8000", // Local development
+	useEmulators: true,
+};
